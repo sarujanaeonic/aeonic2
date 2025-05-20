0c2a30c08212d4e214d9631477f5cace9034046e
aeonictester <alen.kairat@aeonic-intelligence.de>
2025-05-20 14:29:44 +0200

    first commit

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from selenium import webdriver
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 import json
 import time
+import pandas as pd
 
-options = Options()
+options = uc.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("window-size=1920,1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 
-driver = webdriver.Chrome(options=options)
+driver = uc.Chrome(options=options)
 
 def login(username, password):
     login_url = "https://www.freelancermap.de/login"

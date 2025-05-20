affa8ff25277a9152a72728908f95185bce710f6
sarujanaeonic <saru.siva@aeonic-intelligence.de>
2025-05-20 18:20:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("window-size=1920,1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 
 driver = webdriver.Chrome(options=options)
-
+print("uhaaaaaa")
 
 
